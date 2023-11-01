a68ebba083f83def7e122b3437f58dfc91c00339
Geetha Puppala <rohithagundarapu@gmail.com>
2023-10-31 17:16:56 -0700

    fixed code smell

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -1,4 +1,15 @@
-#Code Smells Example for long method
+def calculate_total_price(items):
+    total = 0
+    for item in items:
+        if item.quantity <= 0:
+            continue
+
+        discount = 0.9 if item.price > 100 else 0.95
+        total += item.price * discount
+
+    return total
+
+'''
 def calculate_total_price(items):
     total = 0
     for item in items:
@@ -8,3 +19,4 @@ def calculate_total_price(items):
             else:
                 total += item.price * 0.95
     return total
+'''
